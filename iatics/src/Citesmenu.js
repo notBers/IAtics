62c0252ef75f08ee28e0e9452bbdc66ea74147ce
notBers <109546794+notBers@users.noreply.github.com>
2023-03-20 12:49:52 -0600

    dep

--- iatics/src/Citesmenu.js
+++ iatics/src/Citesmenu.js
@@ -11,7 +11,7 @@ export function Citesmenu() {
       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       <div className="container">
         <Link className="item" id='search' to={'/cites/engine'}><div><img src={gear} className='lupa'/></div>Engine</Link>
-        <Link className="item" id="cites"  to={'/cites/manual'}><div><img src={summarize} className='lupa'/></div>Manual</Link>
+        <Link className="item" id="cites"  to={'/cites/manual'}>Manual</Link>
 
         <Link to={'/'} className='centered'>{"<"}</Link>
       </div>
